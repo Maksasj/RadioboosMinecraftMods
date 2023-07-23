513f18d308c58745812e836f4e599c22eb4e5938
maksasj <radioboos@gmail.com>
2023-07-23 16:08:16 +0300

    client and server proxy

--- BeekeepersSolarPanels/src/main/java/com/radioboos/beekeeperssolarpanels/BeekeepersSolarPanels.java
+++ BeekeepersSolarPanels/src/main/java/com/radioboos/beekeeperssolarpanels/BeekeepersSolarPanels.java
@@ -3,8 +3,10 @@ package com.radioboos.beekeeperssolarpanels;
 import com.radioboos.api.binnie.extrabees.ExtraBeesApi;
 import com.radioboos.api.binnie.extrabees.ExtraBeesCombApi;
 import com.radioboos.api.ic2.IC2Api;
+import com.radioboos.beekeeperssolarpanels.common.CommonProxy;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLLoadEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
@@ -20,23 +22,32 @@ import net.minecraft.item.ItemStack;
         name = "Beekeepers Solar Panels",
         version = BeekeepersSolarPanels.VERSION,
         dependencies = "required-after:IC2@[2.0,);required-after:Forestry@[4.2,);required-after:ExtraBees@[2.0,);required-after:Forge@[9.10,)")
-public class BeekeepersSolarPanels
-{
+public class BeekeepersSolarPanels {
     public static final String MODID = "beekeeperssolarpanels";
     public static final String VERSION = "1.0";
 
+    @SidedProxy(clientSide = "com.radioboos.beekeeperssolarpanels.proxy.ClientProxy", serverSide = "com.radioboos.beekeeperssolarpanels.common.ServerProxy")
+    public static CommonProxy proxy;
+
+    @Mod.Instance("beekeeperssolarpanels")
+    public static BeekeepersSolarPanels instance;
+
+    public BeekeepersSolarPanels() {
+        instance = this;
+    }
+
     @EventHandler
     public void preinit(FMLPreInitializationEvent event) {
-
+        proxy.preInit(event);
     }
 
     @EventHandler
     public void init(FMLInitializationEvent event) {
-
+        proxy.init(event);
     }
 
     @EventHandler
     public void load(FMLLoadEvent event) {
-
+        proxy.load(event);
     }
 }
---
+++ BeekeepersSolarPanels/src/main/java/com/radioboos/beekeeperssolarpanels/proxy/ServerProxy.java
@@ -0,0 +1,25 @@
+package com.radioboos.beekeeperssolarpanels.proxy;
+
+import com.radioboos.beekeeperssolarpanels.common.CommonProxy;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLLoadEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+public class ServerProxy extends CommonProxy {
+    public void preInit(FMLPreInitializationEvent event) {
+        super.preInit(event);
+    }
+
+    public void init(FMLInitializationEvent event) {
+        super.init(event);
+    }
+
+    public void postInit(FMLPostInitializationEvent event) {
+        super.postInit(event);
+    }
+
+    public void load(FMLLoadEvent event) {
+        super.load(event);
+    }
+}

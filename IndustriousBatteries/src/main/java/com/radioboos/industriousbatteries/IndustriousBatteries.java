dc853549cec53b54532201704670deffeb8ef62b
maksasj <radioboos@gmail.com>
2023-07-28 20:36:49 +0300

    Initilized project for industrious batteries

---
+++ IndustriousBatteries/src/main/java/com/radioboos/industriousbatteries/IndustriousBatteries.java
@@ -0,0 +1,28 @@
+package com.radioboos.industriousbatteries;
+
+import cpw.mods.fml.common.event.FMLLoadEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+
+@Mod(modid = IndustriousBatteries.MODID, version = IndustriousBatteries.VERSION)
+public class IndustriousBatteries
+{
+    public static final String MODID = "industriousbatteries";
+    public static final String VERSION = "1.0";
+    
+    @EventHandler
+    public void preinit(FMLPreInitializationEvent event) {
+    }
+
+    @EventHandler
+    public void init(FMLInitializationEvent event) {
+
+    }
+
+    @EventHandler
+    public void load(FMLLoadEvent event) {
+
+    }
+}

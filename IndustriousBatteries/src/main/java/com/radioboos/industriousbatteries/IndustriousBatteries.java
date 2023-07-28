98f20299fd98919b4efae5782b30bbb554cba183
maksasj <radioboos@gmail.com>
2023-07-28 20:41:25 +0300

    typo in proxy path

--- IndustriousBatteries/src/main/java/com/radioboos/industriousbatteries/IndustriousBatteries.java
+++ IndustriousBatteries/src/main/java/com/radioboos/industriousbatteries/IndustriousBatteries.java
@@ -16,11 +16,11 @@ public class IndustriousBatteries
     public static final String VERSION = "1.0";
 
     @SidedProxy(
-            clientSide = "com.radioboos.beekeeperssolarpanels.proxy.ClientProxy",
-            serverSide = "com.radioboos.beekeeperssolarpanels.common.ServerProxy")
+            clientSide = "com.radioboos.industriousbatteries.proxy.ClientProxy",
+            serverSide = "com.radioboos.industriousbatteries.proxy.ServerProxy")
     public static CommonProxy proxy;
 
-    @Mod.Instance("beekeeperssolarpanels")
+    @Mod.Instance("industriousbatteries")
     public static IndustriousBatteries instance;
 
     public IndustriousBatteries() {
